1b6a219d10bef0a6a92bc9286b9f42457bd0c93d
kaushalkantmishra <kaushalkantmishra127@gmail.com>
2024-05-16 10:11:19 +0530

    somthing chnge on js

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-// JavaScript to handle signup form popup
+
 document.getElementById('signup').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-    
-    // Display the signup form popup
+    event.preventDefault(); 
+
     document.getElementById('signupFormPopup').style.display = 'block';
-    
-    // Reset the form fields
     document.getElementById('signupFormContent').reset();
 });
-
-// Close the signup form popup when the close button is clicked
 document.getElementById('closeSignupForm').addEventListener('click', function() {
     document.getElementById('signupFormPopup').style.display = 'none';
 });
 
-// JavaScript to handle form submission
 document.getElementById('signupFormContent').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
     
-    // Display the success message
     document.getElementById('signupSuccessMessage').classList.remove('hidden');
-    
-    // Reset the form fields
     document.getElementById('signupFormContent').reset();
 });
 
